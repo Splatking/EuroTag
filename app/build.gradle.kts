9baf63df1ec15b4ff0feee52b193fa5fd983ed19
Splatking <523553@student.fontys.nl>
2025-04-19 11:05:55 +0200

    Updated the system to actual boot!

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,8 @@ android {
 }
 
 dependencies {
-
+    implementation(libs.navigation.fragment.ktx)
+    implementation(libs.navigation.ui.ktx)
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
